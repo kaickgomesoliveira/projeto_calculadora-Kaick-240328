beceb3a7efb4450ce6eab6d6ffcc24703a0dc4a4
kaick050905 <kaickgo2006@gmail.com>
2026-05-15 09:25:36 -0300

    Refatora classe Calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,27 +5,46 @@ public class Calculadora {
     public int r = 0;
 
     public int calc(int a, int b, String op) {
-
-        //Calculadora
-        if (op.equals("+")) {
-            r = a + b;
-        } else if (op.equals("-")) {
-            r = a - b;
-        } else if (op.equals("*")) {
-            r = a * b;
-        } else if (op.equals("/")) {
-            if (b != 0) {
-                r = a / b;
-            } else {
-                System.out.println("Erro: divisao por zero, operacao irregular");
+        switch (op) {
+            case "+":
+                r = somar(a, b);
+                break;
+            case "-":
+                r = subtrair(a, b);
+                break;
+            case "*":
+                r = multiplicar(a, b);
+                break;
+            case "/":
+                r = dividir(a, b);
+                break;
+            default:
+                System.out.println("Operacao invalida");
                 r = 0;
-            }
-        } else {
-            System.out.println("Operacao invalida");
-            r = 0;
         }
 
         System.out.println("Resultado = " + r);
         return r;
+    }
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Erro: divisao por zero, operacao irregular");
+            return 0;
+        }
     }
 }
