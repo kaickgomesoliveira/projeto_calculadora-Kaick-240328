6591a954ebf8432de2a3cebfda93868c66a584f4
kaick050905 <kaickgo2006@gmail.com>
2026-05-15 09:32:17 -0300

    Refatora classe TesteCalculadora

--- src/calculadora/TesteCalculadora.java
+++ src/calculadora/TesteCalculadora.java
@@ -5,12 +5,22 @@ public class TesteCalculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        // Testes rapidos
+        System.out.println("Teste de soma:");
         calc.calc(2, 3, "+"); // esperado 5
+
+        System.out.println("Teste de subtracao:");
         calc.calc(10, 4, "-"); // esperado 6
+
+        System.out.println("Teste de multiplicacao:");
         calc.calc(3, 5, "*"); // esperado 15
+
+        System.out.println("Teste de divisao:");
         calc.calc(8, 2, "/"); // esperado 4
-        calc.calc(10, 0, "/"); // divisao por zero
+
+        System.out.println("Teste de divisao por zero:");
+        calc.calc(10, 0, "/"); // erro esperado
+
+        System.out.println("Teste de operacao invalida:");
         calc.calc(5, 5, "%"); // operacao invalida
     }
 }
