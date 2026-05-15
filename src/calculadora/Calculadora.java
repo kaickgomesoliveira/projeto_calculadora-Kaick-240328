a46059d111a352f16620215fa9738c6dda418047
kaick050905 <kaickgo2006@gmail.com>
2026-05-15 09:41:39 -0300

    Documenta classe Calculadora com JavaDoc

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,9 +1,33 @@
 package calculadora;
 
+/**
+ * Classe responsavel por realizar operacoes matematicas basicas.
+ * 
+ * A classe Calculadora possui metodos para soma, subtracao,
+ * multiplicacao e divisao, alem de tratar erros como divisao por zero
+ * e operacoes invalidas.
+ * 
+ * @author Kaick
+ * @version 1.0
+ */
 public class Calculadora {
 
+    /**
+     * Atributo que armazena o resultado da operacao realizada.
+     */
     public int r = 0;
 
+    /**
+     * Metodo principal da calculadora.
+     * 
+     * Recebe dois numeros inteiros e uma operacao matematica, executando
+     * o calculo correspondente.
+     * 
+     * @param a primeiro numero da operacao
+     * @param b segundo numero da operacao
+     * @param op operador matematico informado pelo usuario
+     * @return resultado da operacao realizada
+     */
     public int calc(int a, int b, String op) {
         switch (op) {
             case "+":
@@ -27,18 +51,49 @@ public class Calculadora {
         return r;
     }
 
+    /**
+     * Realiza a soma entre dois numeros inteiros.
+     * 
+     * @param a primeiro numero
+     * @param b segundo numero
+     * @return resultado da soma
+     */
     public int somar(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Realiza a subtracao entre dois numeros inteiros.
+     * 
+     * @param a primeiro numero
+     * @param b segundo numero
+     * @return resultado da subtracao
+     */
     public int subtrair(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Realiza a multiplicacao entre dois numeros inteiros.
+     * 
+     * @param a primeiro numero
+     * @param b segundo numero
+     * @return resultado da multiplicacao
+     */
     public int multiplicar(int a, int b) {
         return a * b;
     }
 
+    /**
+     * Realiza a divisao entre dois numeros inteiros.
+     * 
+     * Caso o divisor seja zero, o metodo apresenta uma mensagem de erro
+     * e retorna zero, evitando falhas na execucao do programa.
+     * 
+     * @param a dividendo
+     * @param b divisor
+     * @return resultado da divisao ou zero em caso de divisao por zero
+     */
     public int dividir(int a, int b) {
         if (b != 0) {
             return a / b;
