1b92afb05f2ea619a15d4989ad6aeba11a06040c
Kaick Gomes de Oliveira <59458848+kaickgomesoliveira@users.noreply.github.com>
2026-05-18 13:51:49 -0300

    Update comments for TesteCalculadora class

--- src/calculadora/TesteCalculadora.java
+++ src/calculadora/TesteCalculadora.java
@@ -1,10 +1,11 @@
 package calculadora;
 
 /**
- * Classe responsavel por executar testes simples da classe Calculadora.
+ * Classe utilizada para realizar verificacoes basicas da classe Calculadora.
  * 
- * Os testes verificam as operacoes basicas da calculadora, incluindo
- * soma, subtracao, multiplicacao, divisao, divisao por zero e operacao invalida.
+ * Este programa executa diferentes operacoes matematicas para conferir
+ * o funcionamento dos metodos de soma, subtracao, multiplicacao e divisao,
+ * incluindo tambem casos de erro, como divisao por zero e operador invalido.
  * 
  * @author Kaick
  * @version 1.0
@@ -12,29 +13,29 @@ package calculadora;
 public class TesteCalculadora {
 
     /**
-     * Metodo principal responsavel por executar os testes da calculadora.
+     * Metodo de entrada do programa, onde os testes da calculadora sao chamados.
      * 
-     * @param args argumentos da linha de comando
+     * @param args parametros recebidos pela linha de comando
      */
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
         System.out.println("Teste de soma:");
-        calc.calc(2, 3, "+"); // esperado 5
+        calc.calc(2, 3, "+"); // resultado esperado: 5
 
         System.out.println("Teste de subtracao:");
-        calc.calc(10, 4, "-"); // esperado 6
+        calc.calc(10, 4, "-"); // resultado esperado: 6
 
         System.out.println("Teste de multiplicacao:");
-        calc.calc(3, 5, "*"); // esperado 15
+        calc.calc(3, 5, "*"); // resultado esperado: 15
 
         System.out.println("Teste de divisao:");
-        calc.calc(8, 2, "/"); // esperado 4
+        calc.calc(8, 2, "/"); // resultado esperado: 4
 
         System.out.println("Teste de divisao por zero:");
-        calc.calc(10, 0, "/"); // erro esperado
+        calc.calc(10, 0, "/"); // deve exibir mensagem de erro
 
         System.out.println("Teste de operacao invalida:");
-        calc.calc(5, 5, "%"); // operacao invalida esperada
+        calc.calc(5, 5, "%"); // deve indicar operacao invalida
     }
 }
